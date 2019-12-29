9c4e60d6c8da8e500269d29c3077503b12e2b9a1
unknown <kseniya-bagnyuk@mail.ru>
2019-12-30 00:03:53 +0700

    добавлен функционал автоматической отложенной отправки сообщений

--- handlers.py
+++ handlers.py
@@ -2,9 +2,16 @@ import datetime
 import ephem 
 from glob import glob
 import logging
+import os
 from random import choice
 
-from utils import get_keyboard, get_user_emo
+from telegram import ReplyKeyboardRemove, ReplyKeyboardMarkup, ParseMode
+from telegram.ext import ConversationHandler
+from telegram.ext import messagequeue as mq
+
+from utils import get_keyboard, get_user_emo, is_cat
+
+from bot import subscribers
 
 def greet_user(bot, update, user_data):
     emo = get_user_emo(user_data)
@@ -81,3 +88,89 @@ def get_contact(bot, update, user_data):
 def get_location(bot, update, user_data):
     print(update.message.location)
     update.message.reply_text('Готово: {}'.format(get_user_emo(user_data)), reply_markup=get_keyboard())
+
+def check_user_photo(bot, update, user_data):
+    update.message.reply_text("Обрабатываю фото")
+    os.makedirs('downloads', exist_ok=True)
+    photo_file = bot.getFile(update.message.photo[-1].file_id)
+    filename = os.path.join('downloads', '{}.jpg'.format(photo_file.file_id))
+    photo_file.download(filename)
+    if is_cat(filename):
+        update.message.reply_text("Обнаружен котик, добавляю в библиотеку.")
+        new_filename = os.path.join('images', 'cat_{}.jpg'.format(photo_file.file_id))
+        os.rename(filename, new_filename)
+    else:
+        os.remove(filename)
+        update.message.reply_text("Тревога, котик не обнаружен!")
+
+def anketa_start(bot, update, user_data):
+    update.message.reply_text("Как вас зовут? Напишите имя и фамилию", reply_markup=ReplyKeyboardRemove())
+    return "name"
+
+def anketa_get_name(bot, update, user_data):
+    user_name = update.message.text
+    if len(user_name.split(" ")) != 2:
+        update.message.reply_text("Пожалуйста, введите имя и фамилию")
+        return "name"
+    else:
+        user_data['anketa_name'] = user_name
+        reply_keyboard = [["1", "2", "3", "4", "5"]]
+
+        update.message.reply_text(
+            "Оцените нашего бота от 1 до 5",
+            reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+        return "rating"
+def anketa_rating(bot, update, user_data):
+    user_data['anketa_rating'] = update.message.text
+    update.message.reply_text("""Пожалуйста, напишите отзыв в свободной форме 
+    или нажмите /cancel, чтобы пропустить""")
+    return "comment"
+
+def anketa_comment(bot, update, user_data):
+    user_data["anketa_comment"] = update.message.text
+    text = """
+<b>Фамилия Имя:</b>{anketa_name}
+<b>Оценка:</b>{anketa_rating}
+<b>Комментарий:</b>{anketa_comment}""".format(**user_data)
+    update.message.reply_text(text, reply_markup=get_keyboard(), parse_mode=ParseMode.HTML)
+    return ConversationHandler.END
+
+def anketa_skip_comment(bot, update, user_data):
+    text = """
+<b>Фамилия Имя:</b>{anketa_name}
+<b>Оценка:</b>{anketa_rating}""".format(**user_data)
+    update.message.reply_text(text, reply_markup=get_keyboard(), parse_mode=ParseMode.HTML)
+    return ConversationHandler.END
+
+def dontknow(bot, update, user_data):
+    update.message.reply_text("Не понимаю!")
+
+def subscribe(bot, update):
+    subscribers.add(update.message.chat_id)
+    update.message.reply_text('Вы подписались')
+    print(subscribers)
+
+@mq.queuedmessage
+def send_updates(bot, job):
+    for chat_id in subscribers:
+        bot.sendMessage(chat_id=chat_id, text="BUZZ!")
+
+def unsubscribe(bot, update):  
+    if update.message.chat_id in subscribers:
+        subscribers.remove(update.message.chat_id)
+        update.message.reply_text('Вы отписались')
+    else:
+        update.message.reply_text('Вы не подписаны, нажмите /subscribe, чтобы подписаться')
+
+def set_alarm(bot, update, args, job_queue):
+    try:
+        seconds=abs(int(args[0]))
+        job_queue.run_once(alarm, seconds, context=update.message.chat_id)
+    except(IndexError, ValueError):
+        update.message.reply_text("Введите количество секунд после /alarm")
+
+@mq.queuedmessage
+def alarm(bot, job):
+    bot.sendMessage(chat_id=job.context, text="Сработал будильник")
+
+        
